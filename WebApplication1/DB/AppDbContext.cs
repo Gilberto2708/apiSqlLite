540b92f09ef6d1a1df6f278b9ebaf64e91b13736
Gilberto Cabrera <gilbertocabreravargas@gmail.com>
2025-01-21 18:25:24 -0700

    Agregar archivos de proyecto.

---
+++ WebApplication1/DB/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entity;
+
+namespace WebApplication1.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+         
+        }
+
+        public DbSet<Entity.Person> Persons { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=miDb.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasKey(p => p.Id); // Define la clave primaria
+        }
+
+    }
+
+}
+                                                                                                                              
---
+++ WebApplication1/Entity/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Entity
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
